fda13b5ca6f787cf6f6a453778fe58763fddcbb5
Tammy  Si <tammysi@Tammys-MacBook-Air.local>
2023-01-14 16:21:13 -0800

    Game over and shows winner when game over. Window also clears

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ const int HEIGHT = 1000;
 const int PADDLE_W = 35;
 const int PADDLE_H = 150;
 
+bool gameover = false;
+
 int main()
 {
     int curr_height =  (HEIGHT - PADDLE_H) / 2;
@@ -42,118 +44,135 @@ int main()
 
     while (window.isOpen())
     {
+        window.clear();
         sf::Event event;
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-        {
-            // check to make sure it's not out of bounds
-            if (curr_height > 0) {
-            // up key is pressed: move paddle up
-                curr_height -= 10;
+        if (!gameover) {
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+            {
+                // check to make sure it's not out of bounds
+                if (curr_height > 0) {
+                // up key is pressed: move paddle up
+                    curr_height -= 10;
+                }
+            } 
+            // move the paddle down with the down key
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+                if (curr_height < 850){
+                    curr_height += 10;
+                }
             }
-        } 
-        // move the paddle down with the down key
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            if (curr_height < 850){
-                curr_height += 10;
+
+            window.clear();
+            
+            // check for ball colliding with right paddle (player paddle)
+            // Width of the window -95 because -70 is where the paddle is at and extra -25 for the width of the ball
+            if ((ball_x >= WIDTH - 95) && (ball_x <= 1240) && (ball_y > curr_height-25) && (ball_y < curr_height + PADDLE_H)) {
+                ball_dx = -15;
+                // depending on where the ball hits the paddle decide the y change
+                ball_dy = (ball_y + 12.5 - (2 * curr_height + PADDLE_H)/2)/4;
+            // check for collision with the left paddle
+            // ball_x 60 because of the right edge of the bot paddle met up with ball left edge
+            } else if ((ball_x <= 60) && (ball_x >= 35) && (ball_y > bot_height-25) && (ball_y < bot_height + PADDLE_H)) {
+                ball_dx = 15;
+                ball_dy = (ball_y + 12.5 - (2 * bot_height + PADDLE_H)/2)/4;
+            }
+
+            // bouncing off the ceiling and floor
+            if ((ball_y <= 0) ||  (ball_y >= HEIGHT - 25)){
+                ball_dy = -ball_dy;
+            }  
+
+            // when the ball hits the left edge give the point to the player
+            if (ball_x <= 0) {
+                player_score += 1;
+                // move the ball back into the middle
+                ball_x = (WIDTH - 25) / 2;
+                ball_y = (HEIGHT - 25) /2;
+                ball_dx = -5;
+                ball_dy = 10;
+            // ball hitting the right edge
+            } else if (ball_x >= WIDTH - 25) {
+                bot_score += 1;
+                ball.setPosition((WIDTH - 25) / 2, (HEIGHT - 25) /2);
+                ball_x = (WIDTH - 25) / 2;
+                ball_y = (HEIGHT - 25) /2;      
+                ball_dx = 5;   
+                ball_dy = 10;   
+            }
+
+            // draw out the bot score
+            sf::Text bot_score_display;
+            bot_score_display.setString(to_string(bot_score));
+            bot_score_display.setFont(font);
+            bot_score_display.setFillColor(sf::Color::White);
+            bot_score_display.setPosition(525, 25);
+            bot_score_display.setCharacterSize(75);
+            window.draw(bot_score_display);
+
+            // draw out the player score
+            sf::Text player_score_display;
+            player_score_display.setString(to_string(player_score));
+            player_score_display.setFont(font);
+            player_score_display.setFillColor(sf::Color::White);
+            player_score_display.setPosition(725, 25);
+            player_score_display.setCharacterSize(75);
+            window.draw(player_score_display);
+
+            // draw out paddles
+            bot_paddle.setPosition(Vector2f(35, bot_height));
+            player_paddle.setPosition(Vector2f(WIDTH - 70, curr_height)); 
+            window.draw(player_paddle);
+            window.draw(bot_paddle);
+
+            // draw out the lines in the middle of the board
+            for (int i = 0; i <= HEIGHT; i += 75) {
+                sf::RectangleShape square(Vector2f(25,25));
+                square.setFillColor(sf::Color::White);
+                square.setPosition((WIDTH - 25) / 2, i);
+                window.draw(square);
+            }
+
+            // ball movement
+            ball_x += ball_dx;
+            ball_y += ball_dy;
+            ball.setPosition(ball_x, ball_y);
+            window.draw(ball);
+
+            // bot movement
+            // predict where the ball will be heading to and move the bot paddle based on the prediction
+            int predicted = ball_y - (ball_dy / ball_dx) * ball_x;
+            // try to make it less jittery
+            if (!(predicted > bot_height && predicted < bot_height + PADDLE_H)) {
+                // go up more if needed or down more
+                if ((bot_height < predicted) && (bot_height < 850)){
+                    bot_height += 10;
+                } else if ((bot_height > predicted) && (bot_height > 0)) {
+                    bot_height -= 10;
+                }
+            }
+
+            if ((player_score == 10) || (bot_score == 10)) {
+                gameover = true;
             }
         }
-
-        window.clear();
         
-        // check for ball colliding with right paddle (player paddle)
-        // Width of the window -95 because -70 is where the paddle is at and extra -25 for the width of the ball
-        if ((ball_x >= WIDTH - 95) && (ball_x <= 1240) && (ball_y > curr_height-25) && (ball_y < curr_height + PADDLE_H)) {
-            ball_dx = -15;
-            // depending on where the ball hits the paddle decide the y change
-            ball_dy = (ball_y + 12.5 - (2 * curr_height + PADDLE_H)/2)/4;
-        // check for collision with the left paddle
-        // ball_x 60 because of the right edge of the bot paddle met up with ball left edge
-        } else if ((ball_x <= 60) && (ball_x >= 35) && (ball_y > bot_height-25) && (ball_y < bot_height + PADDLE_H)) {
-            ball_dx = 15;
-            ball_dy = (ball_y + 12.5 - (2 * bot_height + PADDLE_H)/2)/4;
-        }
-
-        // bouncing off the ceiling and floor
-        if ((ball_y <= 0) ||  (ball_y >= HEIGHT - 25)){
-            ball_dy = -ball_dy;
-        }  
-
-        // when the ball hits the left edge give the point to the player
-        if (ball_x <= 0) {
-            player_score += 1;
-            // move the ball back into the middle
-            ball_x = (WIDTH - 25) / 2;
-            ball_y = (HEIGHT - 25) /2;
-            ball_dx = -5;
-            ball_dy = 10;
-        // ball hitting the right edge
-        } else if (ball_x >= WIDTH - 25) {
-            bot_score += 1;
-            ball.setPosition((WIDTH - 25) / 2, (HEIGHT - 25) /2);
-            ball_x = (WIDTH - 25) / 2;
-            ball_y = (HEIGHT - 25) /2;      
-            ball_dx = 5;   
-            ball_dy = 10;   
-        }
-
-        // draw out the bot score
-        sf::Text bot_score_display;
-        bot_score_display.setString(to_string(bot_score));
-        bot_score_display.setFont(font);
-        bot_score_display.setFillColor(sf::Color::White);
-        bot_score_display.setPosition(525, 25);
-        bot_score_display.setCharacterSize(75);
-        window.draw(bot_score_display);
-
-        // draw out the player score
-        sf::Text player_score_display;
-        player_score_display.setString(to_string(player_score));
-        player_score_display.setFont(font);
-        player_score_display.setFillColor(sf::Color::White);
-        player_score_display.setPosition(725, 25);
-        player_score_display.setCharacterSize(75);
-        window.draw(player_score_display);
-
-        // draw out paddles
-        bot_paddle.setPosition(Vector2f(35, bot_height));
-        player_paddle.setPosition(Vector2f(WIDTH - 70, curr_height)); 
-        window.draw(player_paddle);
-        window.draw(bot_paddle);
-
-        // draw out the lines in the middle of the board
-        for (int i = 0; i <= HEIGHT; i += 75) {
-            sf::RectangleShape square(Vector2f(25,25));
-            square.setFillColor(sf::Color::White);
-            square.setPosition((WIDTH - 25) / 2, i);
-            window.draw(square);
-        }
-
-        // ball movement
-        ball_x += ball_dx;
-        ball_y += ball_dy;
-        ball.setPosition(ball_x, ball_y);
-        window.draw(ball);
-
-        // bot movement
-        // predict where the ball will be heading to and move the bot paddle based on the prediction
-        int predicted = ball_y - (ball_dy / ball_dx) * ball_x;
-        // try to make it less jittery
-        if (!(predicted > bot_height && predicted < bot_height + PADDLE_H)) {
-            // go up more if needed or down more
-            if ((bot_height < predicted) && (bot_height < 850)){
-                bot_height += 10;
-            } else if ((bot_height > predicted) && (bot_height > 0)) {
-                bot_height -= 10;
-            }
+        // gameover screen
+        if (gameover) {
+            string winner = (player_score > bot_score) ? "Player " : "Bot ";
+            sf::Text win_display;
+            win_display.setString(winner + "wins");
+            win_display.setFont(font);
+            win_display.setFillColor(sf::Color::White);
+            win_display.setPosition(450, 300);
+            win_display.setCharacterSize(75);
+            window.draw(win_display);
         }
         window.display();
     }
-
     return 0;
 }
